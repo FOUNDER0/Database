c1b9925d4fad0f0c859eca67b9b71d6669455899
Bryar Rebwar <137176897+FOUNDER0@users.noreply.github.com>
2026-04-11 21:07:28 +0300

    Add files via upload

---
+++ connection.php
@@ -0,0 +1,15 @@
+<?php
+$servername = "localhost";
+$username = "root";     // default for XAMPP
+$password = "";         // leave empty unless you added a password
+$database = "database"; // your database name
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $database);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+?>
+<?php
